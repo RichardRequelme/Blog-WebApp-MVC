56c77549ebb87fa427f9a3fc8dac8efe5686c535
richardRequelme2023 <richardrequelme3@gmail.com>
2023-06-16 23:45:11 -0500

    Add project files.

---
+++ Blog.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,41 @@
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly BloggieDbContext bloggieDbContext;
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+
+        [HttpGet]
+        //[Route("/AdminTags/Add")] it's not necessary as in APIs
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            var name = addTagRequest.Name;
+            var displayName = addTagRequest.DisplayName;
+            //Mappigng the AddTagRequest to a Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
+            return View("Add");
+        }
+    }
+}
---
+++ Blog.Web/Data/BloggieDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Web.Data
+{
+    public class BloggieDbContext : DbContext
+    {
+        public BloggieDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        //Relationship many-to-many / The tables to be created
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }    
+
+    }
+}
